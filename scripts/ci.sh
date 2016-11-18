ed9922ab7ff0e75e6c321c7b9ae2ff16db6bb6ea
Björn Dahlgren <bjodah@gmail.com>
2016-11-18 19:10:11 +0100

    Update setup.py

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,18 +3,11 @@ if [[ "$CI_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
     eval export ${1^^}_RELEASE_VERSION=\$CI_BRANCH
     echo ${CI_BRANCH} | tail -c +2 > __conda_version__.txt
 fi
-for PY in python2 python3; do
-    $PY -m pip install --upgrade pip
-    $PY -m pip install --ignore-installed https://github.com/cython/cython/archive/966a296ac4e4862dee9a571ee412886ca3c61144.zip --install-option="--no-cython-compile"
-done
+python2 -m pip install --ignore-installed cython --install-option="--no-cython-compile"
 python2 setup.py sdist
-pip install dist/*.tar.gz
+ORIPATH=$(pwd)
+(cd /; python2 -m pip install $ORIPATH/dist/*.tar.gz)
 (cd /; python2 -m pytest --pyargs $1)
 python2 setup.py build_ext -i
-CFLAGS='-flto' python3 setup.py build_ext -i
-python3 -m pytest
-for PYTHON in python2 python3; do
-    PYTHONPATH=$(pwd) ./scripts/run_tests.sh --cov $1 --cov-report html
-done
+PYTHONPATH=$ORIPATH PYTHON=python3 CFLAGS='-flto' ./scripts/run_tests.sh --cov $1 --cov-report html
 ./scripts/coverage_badge.py htmlcov/ htmlcov/coverage.svg
-! grep "DO-NOT-MERGE!" -R . --exclude ci.sh
