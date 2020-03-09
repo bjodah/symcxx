6fe2e4c7db5c018076504f29d30118f47f984da3
Björn Ingvar Dahlgren <bjodah@gmail.com>
2020-03-09 08:47:57 +0100

    Bump CI

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,13 +1,11 @@
 #!/bin/bash -xeu
-if [[ "$CI_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
+if [[ "$DRONE_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
     eval export ${1^^}_RELEASE_VERSION=\$CI_BRANCH
-    echo ${CI_BRANCH} | tail -c +2 > __conda_version__.txt
 fi
-python2 -m pip install --ignore-installed cython --install-option="--no-cython-compile"
-python2 setup.py sdist
+python3 setup.py sdist
 ORIPATH=$(pwd)
-(cd /; python2 -m pip install $ORIPATH/dist/*.tar.gz)
-(cd /; python2 -m pytest --pyargs $1)
-python2 setup.py build_ext -i
-PYTHONPATH=$ORIPATH PYTHON=python3 CFLAGS='-flto' ./scripts/run_tests.sh --cov $1 --cov-report html
+(cd /; python3 -m pip install $ORIPATH/dist/*.tar.gz)
+(cd /; python3 -m pytest --pyargs $1)
+python3 setup.py build_ext -i
+PYTHONPATH=$ORIPATH PYTHON=python3 ./scripts/run_tests.sh --cov $1 --cov-report html
 ./scripts/coverage_badge.py htmlcov/ htmlcov/coverage.svg
